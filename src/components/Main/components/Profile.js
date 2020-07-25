c9c411d11053916cde0be21c650c2d3d26ae82e9
korenh <korenx123@gmail.com>
2020-07-25 15:34:16 +0300

    24.7.20

--- src/components/Main/components/Profile.js
+++ src/components/Main/components/Profile.js
@@ -8,7 +8,7 @@ export default class Profile extends Component {
       <div className="profile">
         <div className="profile-head">
           <img
-            src="https://firebasestorage.googleapis.com/v0/b/altro-db7f0.appspot.com/o/users%2F1593953149041.jpg?alt=media&token=62bd1a4f-78f6-4a94-b0b6-3b9ecbf27c8a"
+            src={sessionStorage.getItem("url")}
             alt="img"
             className="profile-pic"
           />
