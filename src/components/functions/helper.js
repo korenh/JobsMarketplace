b6042cc3155dd471b70480c7906d8a60e3c4d2a3
korenh <korenx123@gmail.com>
2020-08-06 13:41:47 +0300

    6.8.20withRate

--- src/components/functions/helper.js
+++ src/components/functions/helper.js
@@ -12,5 +12,10 @@ export const addNotification = (notification) => {
 };
 
 export const GeoName = (lng, lat) => {
+  fetch(
+    `https://api.bigdatacloud.net/data/reverse-geocode?latitude=${lat}&longitude=${lng}&localityLanguage=en&key=5305f546fbc84e378acc3138bdd5a82f`
+  )
+    .then((response) => response.json())
+    .then((data) => console.log(data.city));
   return "location";
 };
