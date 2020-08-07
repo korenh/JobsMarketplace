ace66f4cf06bc1acf2c7fe60494a962a99f904d2
korenh <korenx123@gmail.com>
2020-08-07 12:57:53 +0300

    start-fcm

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,4 +1,16 @@
 importScripts("https://www.gstatic.com/firebasejs/5.5.6/firebase-app.js");
 importScripts("https://www.gstatic.com/firebasejs/5.5.6/firebase-messaging.js");
 
+var firebaseConfig = {
+  apiKey: "AIzaSyBbxuTxDeQbqO3Q6EawvRuwoFaelZbto4E",
+  authDomain: "altro-db7f0.firebaseapp.com",
+  databaseURL: "https://altro-db7f0.firebaseio.com",
+  projectId: "altro-db7f0",
+  storageBucket: "altro-db7f0.appspot.com",
+  messagingSenderId: "28461951662",
+  appId: "1:28461951662:web:fb27677313e64e15cd9b3f",
+};
+
+firebase.initializeApp(firebaseConfig);
+
 const messaging = firebase.messaging();
