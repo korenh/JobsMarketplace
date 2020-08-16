66c6113bce9d2171453c58383e496a4a8526203c
korenh <korenx123@gmail.com>
2020-08-16 23:23:33 +0300

    16.8.20

--- src/components/sign/Signin.js
+++ src/components/sign/Signin.js
@@ -27,6 +27,9 @@ export default class Signin extends Component {
             });
           });
         this.props.history.push("/main/jobs");
+        setTimeout(() => {
+          this.props.history.push("/main/jobs");
+        }, 500);
       })
       .catch(function (error) {
         var errorMessage = error.message;
