36df5cf5845291015c5236ba565ae93886fee8e7
korenh <korenx123@gmail.com>
2020-09-08 23:16:04 +0300

    WithEnv

--- src/components/protected/Firebase.js
+++ src/components/protected/Firebase.js
@@ -6,7 +6,8 @@ import "firebase/firestore";
 import "firebase/messaging";
 
 var firebaseConfig = {
-  apiKey: process.env.REACT_APP_SECRET,
+  apiKey:
+    process.env.REACT_APP_SECRET || "AIzaSyBbxuTxDeQbqO3Q6EawvRuwoFaelZbto4E",
   authDomain: "altro-db7f0.firebaseapp.com",
   databaseURL: "https://altro-db7f0.firebaseio.com",
   projectId: "altro-db7f0",
