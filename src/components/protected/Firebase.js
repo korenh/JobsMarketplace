c5df0ccedc4a1d5068415eadacac5049aa5cb2c9
koren hamra <56797632+korenh@users.noreply.github.com>
2021-01-04 09:12:55 +0200

    Update Firebase.js

--- src/components/protected/Firebase.js
+++ src/components/protected/Firebase.js
@@ -6,14 +6,7 @@ import "firebase/firestore";
 import "firebase/messaging";
 
 var firebaseConfig = {
-  apiKey:
-    process.env.REACT_APP_SECRET || "AIzaSyBbxuTxDeQbqO3Q6EawvRuwoFaelZbto4E",
-  authDomain: "altro-db7f0.firebaseapp.com",
-  databaseURL: "https://altro-db7f0.firebaseio.com",
-  projectId: "altro-db7f0",
-  storageBucket: "altro-db7f0.appspot.com",
-  messagingSenderId: "28461951662",
-  appId: "1:28461951662:web:fb27677313e64e15cd9b3f",
+  "firebaseConfig"
 };
 
 firebase.initializeApp(firebaseConfig);
